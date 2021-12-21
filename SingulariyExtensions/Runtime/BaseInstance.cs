550a4b3b962e34239b6967e89badbaacf832f22f
Silentdesu <silentdesu@gmail.com>
2021-12-21 13:38:50 +0600

    Add files via upload

---
+++ SingulariyExtensions/Runtime/BaseInstance.cs
@@ -0,0 +1,45 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Singularity.Scripts.Utils
+{
+    [DefaultExecutionOrder(-100)]
+    public class BaseInstance<T> : SerializedMonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = (T) FindObjectOfType(typeof(T));
+                    
+                    if (_instance == null)
+                    {
+                        DebugX.Warning($"{typeof(T).FullName} Instance NOT READY!");
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance != null)
+            {
+                DebugX.Warning($"{typeof(T).FullName} - Duplicate Instance destroyed!");
+                Destroy(this.gameObject);
+                return;
+            }
+            
+            _instance = this as T;
+            
+            DebugX.Success($"{typeof(T).FullName} Instance Ready!");
+
+        }
+
+    }
+}
