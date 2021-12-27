e1d12ed887bf98df8b0788ecace45fe2174eab85
Silentdesu <silentdesu@gmail.com>
2021-12-27 15:29:12 +0600

    Added LTG

---
+++ Editor/LazyTemplateGenerator.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Singularity.Scripts.Utils.Editor
+{
+    sealed class LazyTemplateGenerator : ScriptableObject
+    {
+        [MenuItem("Assets/Create/LazyTemplate", false, -200)]
+        private static void CreateLazyTemplate () 
+        {
+            var assetPath = GetAssetPath();
+            
+            CreateEmptyFolder($"{assetPath}/_Scenes");
+            CreateEmptyFolder($"{assetPath}/Prefabs");
+            CreateEmptyFolder($"{assetPath}/Materials");
+            CreateEmptyFolder($"{assetPath}/Scripts");
+            
+            AssetDatabase.Refresh();
+        }
+
+        private static string GetAssetPath () 
+        {
+            var path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            
+            if (!string.IsNullOrEmpty(path) && AssetDatabase.Contains(Selection.activeObject)) 
+            {
+                if (!AssetDatabase.IsValidFolder(path)) 
+                {
+                    path = Path.GetDirectoryName(path);
+                }
+            } 
+            else 
+            {
+                path = "Assets";
+            }
+            
+            return path;
+        }
+
+        private static void CreateEmptyFolder (string folderPath) 
+        {
+            if (!Directory.Exists(folderPath)) 
+            {
+                try 
+                {
+                    Directory.CreateDirectory(folderPath);
+                } 
+                catch 
+                {
+                    // 
+                }
+            }
+        }
+    }
+}
