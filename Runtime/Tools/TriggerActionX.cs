8a52167e4e1006e15f013b6be77aeb9785d330ce
Silentdesu <silentdesu@gmail.com>
2022-04-21 23:04:38 +0600

    Added TriggerCustomAction and TriggerActionX

---
+++ Runtime/Tools/TriggerActionX.cs
@@ -0,0 +1,20 @@
+﻿namespace SingularityLab.Runtime.Tools
+{
+    public struct TriggerActionX
+    {
+        public ActionX<bool> OnChange { get; }
+
+        public bool Value { get; private set; }
+
+        public void Set(in bool value)
+        {
+            if (Value == value) return;
+
+            Value = value;
+
+            OnChange.InvokeSafe(Value);
+        }
+
+        public static implicit operator bool(TriggerActionX trigger) => trigger.Value;
+    }
+}
---
+++ Runtime/Tools/TriggerCustomAction.cs
@@ -0,0 +1,19 @@
+﻿namespace SingularityLab.Runtime.Tools
+{
+    public partial class TriggerCustomAction
+    {
+        public CustomAction<bool> OnChange { get; } = new();
+        public bool Value { get; private set; } = false;
+
+        public void Set(in bool value)
+        {
+            if (Value == value) return;
+
+            Value = value;
+
+            OnChange.Invoke(Value);
+        }
+
+        public static implicit operator bool(TriggerCustomAction trigger) => trigger.Value;
+    }
+}
